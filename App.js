0b5375fe850a4a01f24b6f85124bbe4b0684e466
AsHansen88 <Ande970s@stud.kea.dk>
2023-09-13 14:14:49 +0200

    :)

--- App.js
+++ App.js
@@ -3,24 +3,19 @@ import { useState } from 'react';
 import { StyleSheet, TextInput, View, Button, FlatList, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import { app } from './firebase'
 
 export default function App() {
 
   const Stack = createNativeStackNavigator()
-  const [text, setText] = useState('')
-  //const notes = [{key:1, name: "Anna"}, {key:2, name: "Bob"}]
-   //const [notes, setNotes] = useState([])
+    //const notes = [{key:1, name: "Anna"}, {key:2, name: "Bob"}]
+  
 
 
-  function buttonHandler(){
-    //alert ("You Typed: " + text)
-    setNotes(
-      [...notes, {key:notes.length, name: text}]
-    )
-  }
+   //alert (JSON.stringify(app, null, 4))
 
-    return (
+
+      return (
     <NavigationContainer>
 
       <Stack.Navigator initialRouteName='ListPage'>     
@@ -43,12 +38,24 @@ export default function App() {
 const ListPage = ({navigation, route}) => {
   
   const myList = [{key:1, name: "Anna"}, {key:2, name: "Bob"}]
-  
+  const [notes, setNotes] = useState([])
+  const [text, setText] = useState('')
+
+
     function handleButon(item){
 
     navigation.navigate('DetailPage', {message:item})
 
   }
+
+  function buttonHandler(item){
+    //alert ("You Typed: " + text)
+    setNotes(
+      [...notes, {key:notes.length, name: text}]
+    )
+    navigation.navigate('DetailPage', {note:item})
+    }
+
 
   return(
     <View>
@@ -56,7 +63,13 @@ const ListPage = ({navigation, route}) => {
       <Button title ='DetailPage' onPress={handleButon}></Button>
       <FlatList
       data={myList}
-      renderItem={(note) => <Button title = {note.item.name} onPress={() => handleButon(note.item)}></Button>} 
+      renderItem={(note) => <Button title = {note.item.name} onPress={() => handleButon(note.item)}></Button>}
+      />
+      <TextInput style={styles.TextInput} onChangeText={(txt) => setText(txt)} />
+      <Button title='press me' onPress={handleButon}></Button>
+      <FlatList
+      data={notes}
+      renderItem={(note) => <Button title = {note.item.name} onPress={() => buttonHandler(note.item)}></Button>}
       />
     </View>
   )
@@ -65,10 +78,13 @@ const ListPage = ({navigation, route}) => {
 const DetailPage = ({navigation, route}) => {
 
   const message = route.params?.message
+  const note = route.params?.message
+
 
   return(
     <View>
       <Text>Detaljer {message.name}</Text>
+      <Text>Notes {note.name}</Text>
     </View>
   )
 }
---
+++ firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAPe4QjUbc0OaTb-0SaI_uSr2GGl19B6jo",
+  authDomain: "ashansen88mobilenotebook.firebaseapp.com",
+  projectId: "ashansen88mobilenotebook",
+  storageBucket: "ashansen88mobilenotebook.appspot.com",
+  messagingSenderId: "956181951358",
+  appId: "1:956181951358:web:8af7e601b8839ff06b792f",
+  measurementId: "G-Y4RMD8V1HV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export {app}
