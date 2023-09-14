f73a403c70e3d60cad283e51e47568b17318af95
AsHansen88 <Ande970s@stud.kea.dk>
2023-09-14 13:26:44 +0200

    :)

--- App.js
+++ App.js
@@ -1,99 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, TextInput, View, Button, FlatList, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, FlatList, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { app, database } from './firebase'
-import { collection, addDoc } from 'firebase/firestore';
-import { useCollection } from 'react-firebase-hooks/firestore'
-
+import { app, database } from './firebase';
+import { collection, addDoc, deleteDoc, doc } from 'firebase/firestore';
+import { useCollection } from 'react-firebase-hooks/firestore';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
 
-  const Stack = createNativeStackNavigator()
-    //const notes = [{key:1, name: "Anna"}, {key:2, name: "Bob"}]
-  
-
-
-   //alert (JSON.stringify(app, null, 4))
-
-
-      return (
+  return (
     <NavigationContainer>
-
-      <Stack.Navigator initialRouteName='ListPage'>     
-      <Stack.Screen 
-      name='list' 
-      component={ListPage}
-      />
-      
-      <Stack.Screen 
-      name='DetailPage' 
-      component={DetailPage}
-      />
-
+      <Stack.Navigator initialRouteName='ListPage'>
+        <Stack.Screen name='list' component={ListPage} />
+        <Stack.Screen name='DetailPage' component={DetailPage} />
       </Stack.Navigator>
-           
-      </NavigationContainer>
+    </NavigationContainer>
   );
 }
 
-const ListPage = ({navigation, route}) => {
-  
-  const myList = [{key:1, name: "Anna"}, {key:2, name: "Bob"}]
-  const [notes, setNotes] = useState([])
-  const [text, setText] = useState('')
-  const [values, loading, error] = useCollection(collection(database, "notes"))
-  const data = values?.docs.map((doc) => ({...doc.data(), id: doc.id}))
+const ListPage = ({ navigation }) => {
+  const [text, setText] = useState('');
+  const [values, loading, error] = useCollection(collection(database, 'notes'));
+  const data = values?.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
 
-
-    function handleButon(item){
-
-    navigation.navigate('DetailPage', {message:item})
-
+  async function buttonHandler() {
+    try {
+      await addDoc(collection(database, 'notes'), {
+        text: text,
+      });
+      setText('');
+    } catch (err) {
+      console.log('Fejl i DB' + err);
+    }
   }
 
-  async function buttonHandler(){
-    try{
-    await addDoc(collection(database, "notes"), {
-      text: text
-    })
-    }catch(err){
-      console.log("Fejl i DB" + err)
+  async function deleteDocument(id) {
+    try {
+      await deleteDoc(doc(database, 'notes', id));
+    } catch (err) {
+      console.log('Error deleting document:', err);
     }
-    
-    }
+  }
 
-
-  return(
+  return (
     <View>
-      <Text>Hej</Text>
-      <Button title ='DetailPage' onPress={handleButon}></Button>
-      <FlatList
-      data={myList}
-      renderItem={(note) => <Button title = {note.item.name} onPress={() => handleButon(note.item)}></Button>}
+      <Text>My Notes</Text>
+      <TextInput
+        style={styles.TextInput}
+        onChangeText={(txt) => setText(txt)}
+        placeholder='Enter your note'
+        value={text}
       />
-      <TextInput style={styles.TextInput} onChangeText={(txt) => setText(txt)} />
-      <Button title='press me' onPress={buttonHandler}></Button>
+      <Button title='Add Note' onPress={buttonHandler} />
       <FlatList
-      data={data}
-      renderItem={(note) => <Text title = {note.item.text}></Text>}
+        data={data}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.text}</Text>
+            <Button title='Delete' onPress={() => deleteDocument(item.id)} />
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
       />
     </View>
-  )
-}
+  );
+};
 
-const DetailPage = ({ route}) => {
+const DetailPage = ({ route }) => {
+  const message = route.params?.message;
 
-  const message = route.params?.message
- 
-  return(
+  return (
     <View>
       <Text>Detaljer {message.text}</Text>
     </View>
-  )
-}
-
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -101,10 +83,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: 200
+    marginTop: 200,
   },
   TextInput: {
-    backgroundColor: 'lightblue',
-    minWidth: 200
-  }
+    backgroundColor: 'white',
+    minWidth: 200,
+  },
 });
